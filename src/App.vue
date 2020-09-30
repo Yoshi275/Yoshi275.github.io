3d7a132b8bb39b566db2821afeedfcf540f226b6
Cheryl <cheryl.nqj@gmail.com>
2020-09-30 16:26:25 +0800

    Update homepage

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="main">
+    <h1>cheryl-ng</h1>
+    <!-- <p>about me</p>
+    <p>projects</p>
+    <p>resume</p> -->
+    <ul>
+      <li><a href="https://www.linkedin.com/in/cherylnqj/" target="_blank" rel="noopener">about me</a></li>
+      <li><a href="https://github.com/Yoshi275" target="_blank" rel="noopener">projects</a></li>
+      <li><a href="/resume" target="_blank">resume</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto Mono', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  /* display: inline-block; */
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
